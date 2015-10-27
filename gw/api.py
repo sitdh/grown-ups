fa4cf9a6b66c0a07c879eca8843897e20d25eab8
Sitdhibong Laokok <sitdhibong@gmail.com>
2015-10-27 20:43:14 +0700

    Change api structure

--- gw/api.py
+++ gw/api.py
@@ -1,7 +1,22 @@
 # -*- code: utf-8 -*-
 
 class TwitterCredential:
-    CONSUMER_KEY        = ''
-    CONSUMER_SECRET     = ''
-    OAUTH_TOKEN         = ''
-    OAUTH_TOKEN_SECRET  = ''
+
+    def __init__(self):
+        pass
+
+    def credential(self, switcher = False):
+        if switcher:
+            return {
+                    'consumer_key': '',
+                    'consumer_secret': '',
+                    'oauth_token': '',
+                    'oauth_token_secret': ''
+                    }
+        else:
+            return {
+                    'consumer_key': '',
+                    'consumer_secret': '',
+                    'oauth_token': '',
+                    'oauth_token_secret': ''
+                    }
